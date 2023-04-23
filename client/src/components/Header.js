116811a495637f87d7c078925ea0daa4dafe2e48
Kccarr6022 <Kccarr6022@eagle.fgcu.edu>
2023-04-22 21:24:49 -0400

    Progress in UI

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="h-[200px] bg-red-400 flex flex-col items-center justify-center">
-      <h1 className="text-6xl text-yellow-100 font-semibold text-center">
+    <div className="h-[200px] bg-blue-400 flex flex-col items-center justify-center">
+      <h1 className="text-6xl text-white font-semibold text-center">
         Internship Database
       </h1>
     </div>
--- client/src/pages/InternView.js
+++ client/src/pages/InternView.js
@@ -3,6 +3,29 @@ import axios from "axios";
 import Header from "./../components/Header";
 
 const InternView = () => {
+  const [search, setSearch] = useState("");
+  const [addIntenForm, setAddInternForm] = useState(false);
+  const [addInten, setAddIntern] = useState({
+    name: "",
+    gradePointAverage: "",
+    studentEmail: "",
+    internshipName: "",
+    beginDate: "",
+    endDate: "",
+    companyName: "",
+    companyEmail: "",
+  });
+  const [editInternForm, setEditInternForm] = useState(false);
+  const [internInEdit, setInternInEdit] = useState({
+    name: "",
+    gradePointAverage: "",
+    studentEmail: "",
+    internshipName: "",
+    beginDate: "",
+    endDate: "",
+    companyName: "",
+    companyEmail: "",
+  });
   const [studentsView, setStudentsView] = useState([
     {
       name: "Kaden Carr",
@@ -15,7 +38,6 @@ const InternView = () => {
       companyEmail: "wad",
     },
   ]);
-  const [search, setSearch] = useState("");
 
   const handleEnter = (e) => {
     if (e.key === "Enter") {
@@ -36,6 +58,39 @@ const InternView = () => {
       setStudentsView(response.data);
     });
   };
+
+  const addIntern = async () => {
+    setStudentsView([...studentsView, addInten]);
+    axios
+      .post("http://localhost:5000/students_view", { addInten })
+      .then((response) => {
+        setStudentsView(response.data);
+      });
+      setAddInternForm(false);
+  };
+
+  const editIntern = async (intern) => {
+    setEditInternForm(true);
+    setInternInEdit(intern)
+  };
+
+  const updateIntern = async () => {
+    axios
+      .put("http://localhost:5000/students_view", { internInEdit })
+      .then((response) => {
+        setStudentsView(response.data);
+      });
+  };
+
+  const deleteIntern = async (id) => {
+    setStudentsView(studentsView.filter((student) => student.id !== id));
+    axios
+      .delete(`http://localhost:5000/students_view/${id}`)
+      .then((response) => {
+        setStudentsView(response.data);
+      });
+  };
+
 
   useEffect(() => {
     getInterns();
@@ -92,25 +147,214 @@ const InternView = () => {
                 <td>{student.companyName}</td>
                 <td>{student.companyEmail}</td>
                 <td>
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                        Edit
-                    </button>
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+                  onClick={() => editIntern(student)}
+                  >
+                    Edit
+                  </button>
                 </td>
                 <td>
-                    <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full">
-                        Delete
-                    </button>
+                  <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full"
+                  onClick={
+                    () => deleteIntern(student.id)
+                  }>
+                    Delete
+                  </button>
                 </td>
               </tr>
             ))}
             <tr className="">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                    Add Intern
-                </button>
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+                onClick={() => setAddInternForm(!addIntenForm)}
+              >
+                Add Intern
+              </button>
             </tr>
           </table>
         </div>
       </div>
+      {addIntenForm && (
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/2 h-1/2 bg-white z-50 shadow-lg rounded-xl">
+          <button className="absolute top-0 left-0 px-4 py-2 text-2xl bg-red-600" onClick={() => setAddInternForm(!addIntenForm)}>X</button>
+          <h1 className="text-4xl font-semibold text-center">
+            Add Intern
+          </h1>
+          <form onSubmit={
+            (e) => {
+              e.preventDefault();
+              addIntern();
+            }
+          }>
+            <div className="grid lg:grid-cols-2 gap-4 mt-10 mx-5">
+            <div className="flex flex-row items-center my-2">
+              <input
+                type="text"
+                className="ml-4 pl-5 border-solid border-blue-black border- rounded-2xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] h-12 w-full"
+                placeholder="Name"
+                onChange={(e) => setAddIntern({ ...addInten, name: e.target.value })}
+              />
+            </div>
+            <div className="flex flex-row items-center my-2"> 
+              <input
+                type="text"
+                placeholder="GPA"
+                className="ml-4 pl-5 border-solid border-blue-black border- rounded-2xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] h-12 w-full"
+                onChange={(e) => setAddIntern({ ...addInten, gpa: e.target.value })}
+              />
+            </div>
+            <div className="flex flex-row items-center my-2"> 
+              <input
+                type="text"
+                placeholder="Email"
+                className="ml-4 pl-5 border-solid border-blue-black border- rounded-2xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] h-12 w-full"
+                onChange={(e) => setAddIntern({ ...addInten, email: e.target.value })}
+              />
+            </div>
+            <div className="flex flex-row items-center my-2"> 
+              <input
+                type="text"
+                placeholder="Company"
+                className="ml-4 pl-5 border-solid border-blue-black border- rounded-2xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] h-12 w-full"
+                onChange={(e) => setAddIntern({ ...addInten, internshipName: e.target.value })}
+              />
+            </div>
+            <div className="flex flex-row items-center my-2"> 
+              <input
+                type="text"
+                placeholder="Position"
+                className="ml-4 pl-5 border-solid border-blue-black border- rounded-2xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] h-12 w-full"
+                onChange={(e) => setAddIntern({ ...addInten, startDate: e.target.value })}
+              />
+            </div>
+            <div className="flex flex-row items-center my-2"> 
+              <input
+                type="text"
+                placeholder="Start Date"
+                className="ml-4 pl-5 border-solid border-blue-black border- rounded-2xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] h-12 w-full"
+                onChange={(e) => setAddIntern({ ...addInten, endDate: e.target.value })}
+              />
+            </div>
+            <div className="flex flex-row items-center my-2"> 
+              <input
+                type="text"
+                placeholder="End Date"
+                className="ml-4 pl-5 border-solid border-blue-black border- rounded-2xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] h-12 w-full"
+                onChange={(e) => setAddIntern({ ...addInten, companyName: e.target.value })}
+              />
+            </div>
+            <div className="flex flex-row items-center my-2"> 
+              <input
+                type="text"
+                placeholder="Supervisor"
+                className="ml-4 pl-5 border-solid border-blue-black border- rounded-2xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] h-12 w-full"
+                onChange={(e) => setAddIntern({ ...addInten, companyEmail: e.target.value })}
+              />
+            </div>
+            </div>
+            <div className="flex justify-center">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-48 mx-autolex flex-col gap-4 my-12">
+              Submit
+            </button>
+            </div>
+          </form>
+        </div>
+      )}
+      {editInternForm && 
+      (
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/2 h-1/2 bg-white z-50 shadow-lg rounded-xl">
+          <button className="absolute top-0 left-0 px-4 py-2 text-2xl bg-red-600" onClick={() => setEditInternForm(!editInternForm)}>X</button>
+          <h1 className="text-4xl font-semibold text-center">
+            Edit Intern Data
+          </h1>
+          <form onSubmit={
+            (e) => {
+              e.preventDefault();
+              updateIntern();
+            }
+          }>
+            <div className="grid lg:grid-cols-2 gap-4 mt-10 mx-5">
+            <div className="flex flex-row items-center my-2">
+              <input
+                type="text"
+                className="ml-4 pl-5 border-solid border-blue-black border- rounded-2xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] h-12 w-full"
+                placeholder="Name"
+                value={internInEdit.name}
+                onChange={(e) => setInternInEdit({ ...internInEdit, name: e.target.value })}
+              />
+            </div>
+            <div className="flex flex-row items-center my-2"> 
+              <input
+                type="text"
+                placeholder="GPA"
+                value={internInEdit.gradePointAverage}
+                className="ml-4 pl-5 border-solid border-blue-black border- rounded-2xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] h-12 w-full"
+                onChange={(e) => setInternInEdit({ ...internInEdit, gpa: e.target.value })}
+              />
+            </div>
+            <div className="flex flex-row items-center my-2"> 
+              <input
+                type="text"
+                placeholder="Student Email"
+                value={internInEdit.studentEmail}
+                className="ml-4 pl-5 border-solid border-blue-black border- rounded-2xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] h-12 w-full"
+                onChange={(e) => setInternInEdit({ ...internInEdit, email: e.target.value })}
+              />
+            </div>
+            <div className="flex flex-row items-center my-2"> 
+              <input
+                type="text"
+                placeholder="Company"
+                value={internInEdit.internshipName}
+                className="ml-4 pl-5 border-solid border-blue-black border- rounded-2xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] h-12 w-full"
+                onChange={(e) => setInternInEdit({ ...internInEdit, internshipName: e.target.value })}
+              />
+            </div>
+            <div className="flex flex-row items-center my-2"> 
+              <input
+                type="text"
+                placeholder="Start Date"
+                value={internInEdit.startDate}
+                className="ml-4 pl-5 border-solid border-blue-black border- rounded-2xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] h-12 w-full"
+                onChange={(e) => setInternInEdit({ ...internInEdit, startDate: e.target.value })}
+              />
+            </div>
+            <div className="flex flex-row items-center my-2"> 
+              <input
+                type="text"
+                placeholder="End Date"
+                value={internInEdit.endDate}
+                className="ml-4 pl-5 border-solid border-blue-black border- rounded-2xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] h-12 w-full"
+                onChange={(e) => setInternInEdit({ ...internInEdit, endDate: e.target.value })}
+              />
+            </div>
+            <div className="flex flex-row items-center my-2"> 
+              <input
+                type="text"
+                placeholder="End Date"
+                value={internInEdit.companyName}
+                className="ml-4 pl-5 border-solid border-blue-black border- rounded-2xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] h-12 w-full"
+                onChange={(e) => setInternInEdit({ ...internInEdit, companyName: e.target.value })}
+              />
+            </div>
+            <div className="flex flex-row items-center my-2"> 
+              <input
+                type="text"
+                placeholder="Company Email"
+                value={internInEdit.companyEmail}
+                className="ml-4 pl-5 border-solid border-blue-black border- rounded-2xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] h-12 w-full"
+                onChange={(e) => setInternInEdit({ ...internInEdit, companyEmail: e.target.value })}
+              />
+            </div>
+            </div>
+            <div className="flex justify-center">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-48 mx-autolex flex-col gap-4 my-12">
+              Submit
+            </button>
+            </div>
+          </form>
+        </div>
+      )}
     </>
   );
 };
